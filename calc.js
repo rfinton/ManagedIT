d237262a3eedeebb6fba508842b27c2e44e2e046
rfinton@outlook.com <rfinton@outlook.com>
2016-02-11 13:10:11 -0600

    updates

--- calc.js
+++
@@ -1,5 +0,0 @@
-﻿var perc = ##q1##;
-var totalScore = ##q2## + ##q3## + ##q4## + ##q5## + ##q6## + ##q7##;
-var gap = perc - totalScore;
-
-document.getElementById('gap').setAttribute('value', gap);
